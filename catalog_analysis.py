81c88dafcf048fd6eaa28b86d9823b58b78f09bb
Tom McClintock <tmcclintock@email.arizona.edu>
2016-05-19 20:21:13 -0700

    Started organizing the directories. Created the file that will be the front end.

---
+++ catalog_analysis.py
@@ -0,0 +1,17 @@
+"""
+This is the top level object that contains the interface to the 
+catalog analysis code. This code recieves a catalog and a variety 
+of possible options and performs certain calculations on them
+according to the user's specification.
+
+For more information visit:
+https://github.com/tmcclintock/catalog_analysis
+"""
+
+class catalog_analysis(object):
+    def __init__(self):
+        #TODO
+        return
+
+if __name__ == '__main__':
+    print "TODO"
