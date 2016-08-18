0b74344c0ccd5f5624a822d4a1392fd127dca8d3
Tom McClintock <tmcclintock@email.arizona.edu>
2016-08-18 10:00:06 -0700

    Added the jk mapping. Now have all test files assembled.

--- catalog_analysis.py
+++ catalog_analysis.py
@@ -14,6 +14,7 @@ scipy
 """
 
 import os
+import numpy as np
 
 class catalog_analysis(object):
     """
@@ -29,11 +30,11 @@ class catalog_analysis(object):
 
         halo_file: the file that contains the halos identified by rockstar.
         """
-        self.dm_files       = dm_files
+        self.dm_files      = dm_files
         self.halo_file     = halo_file
         self.treecorr_dict = treecorr_dict
         self.flow_control  = flow_control
-        self.out_path     = out_path
+        self.out_path      = out_path
         return
 
     def build_WL_signal(self):
@@ -42,8 +43,9 @@ class catalog_analysis(object):
         """
         self.path_check()
         self.find_simulation_properties()
-        #self.reorder_jackknifes()
+        self.reorder_jackknifes()
         #self.sort_halos()
+        #self.make_randoms()
         #self.run_treecorr()
         #self.resum_correlation_functions()
         #self.build_delta_sigmas()
@@ -55,15 +57,30 @@ class catalog_analysis(object):
         and halo data exist.
         """
         print "Checking DM and halo paths."
+        parts = self.dm_files.split("/")
+        self.dm_filename = parts[-1]
+        if len(parts)==1:
+            self.dm_path = "./"
+        else:
+            self.dm_path = parts[0]
+            for p in parts[1:-1]:
+                self.dm_path = self.dm_path+"/"+p
         #Check that the files exist
-        if not os.path.exists(self.dm_files):
-            raise Exception("Error: dark matter files don't exst.")
+        if not os.path.exists(self.dm_path):
+            raise Exception("dark matter path don't exst.")
         if not os.path.exists(self.halo_file):
-            raise Exception("Error: halo files don't exst.")
+            raise Exception("halo files don't exst.")
         print "\tDM and halo paths exist."
         return
 
     def find_simulation_properties(self):
+        """
+        This finds the attributes of the simulation such as
+        side length, number of DM paticles, number of DM JK files,
+        the redshift, the hubble constant.
+        TODO: figure out the cosmology.
+        """
+        print "Reading the snapshot header."
         import pygadgetreader as pgr
         header = pgr.readheader(self.dm_files,"header")
         self.pgrheader = header
@@ -73,9 +90,36 @@ class catalog_analysis(object):
         self.ndm_jks = header['nfiles']
         self.redshift = header['redshift']
         self.hubble_const = header['h']
-        print header
+        print "\tSnapshot header read. Simulation properties saved."
         return
 
+    def reorder_jackknifes(self):
+        """
+        This reads in the snapshot DM JK files and
+        creates a dictionary that pairs the LGADGET JK number
+        with our ordering.
+        """
+        print "Finding the mapping of LGADGET jackknifes to our jackknifes."
+        import pygadgetreader as pgr
+        N = self.ndm_jks #Number of LGADGET JK files
+        ndivs = int(round(N**(1./3.)))
+        self.ndm_ndivs = ndivs
+        side = self.side_length
+        step = side/ndivs
+        mapping = {}
+        for i in xrange(0,2): #N):
+            pos = pgr.readsnap(self.dm_files+".%d"%i,"pos","dm",single=True,suppress=True)
+            xi,yi,zi = [int(q) for q in np.mean(pos,0)/step]
+            jkindex = zi + ndivs*yi + ndivs*ndivs*xi
+            mapping[jkindex] = i
+        self.mapping = mapping
+        print "\tMapping complete on %d files."%N
+        return
+
+    def sort_halos(self):
+        out_path = self.out_path
+        
+
 if __name__ == '__main__':
-    test = catalog_analysis("test_data/","test_data/")
+    test = catalog_analysis("test_data/dm_files/snapshot_000","test_data/halo_files/outbgc2_0.list")
     test.build_WL_signal()
